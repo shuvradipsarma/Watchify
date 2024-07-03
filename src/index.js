d9c29ccc88626f4fbac0d174e65d8bb1cbbd21a5
IMON <shuvradip.sarma2021@vitbhopal.ac.in>
2024-07-04 00:46:22 +0530

    commit successful

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ dotenv.config({
     path:"./env"
 })
 
-connectDB() 
-
+connectDB().then( ()=>{
+    app.listen(prcoess.env.PORT || 3000, ()=>{
+    console.log(`Server is running at port ${process.env.PORT}`)});
+}).catch((err)=>{
+    console.log("MongoDB connection fail!")
+})
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message="Something went wrong",
+        errors=[],
+        stack=""
+    )
+    {
+        //overriding methods from Error class
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = this.errors
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
