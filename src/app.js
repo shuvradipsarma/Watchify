05cb6fc1397e63f44a2387797cfa00c373f34734
IMON <shuvradip.sarma2021@vitbhopal.ac.in>
2024-07-15 11:46:59 +0530

    utils

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ app.use(cors({
     credentials:true
 }))
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit:"16kb"})) // Parses incoming JSON requests and limits the request body size to 16kb.
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // Parses URL-encoded data with extended option for rich objects and arrays, and limits the request body size to 16kb.
+app.use(express.static("public")) // Serves static files from the "public" directory.
+app.use(cookieParser())  // Parses cookies attached to the client request object.
 
 export {app}
